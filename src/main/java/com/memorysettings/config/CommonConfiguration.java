e610188eb7357cb9b9cf7cec0850de2f06603bcb
someaddons <someaddons@github.com>
2021-11-17 12:39:54 +0100

    v1.8: Update config

--- src/main/java/com/memorysettings/config/CommonConfiguration.java
+++ src/main/java/com/memorysettings/config/CommonConfiguration.java
@@ -16,24 +16,24 @@ public class CommonConfiguration
     {
         builder.push("Config category");
 
-        builder.comment("Set the client minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
+        builder.comment("Set the clients minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
         minimumClient = builder.defineInRange("minimumClient", 2500, 2500, 10000);
 
-        builder.comment("Set the client maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
+        builder.comment("Set the clients maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
                           + "recommended is 6000mb then set this to ~8000mb");
-        maximumClient = builder.defineInRange("maximumClient", 8500, 2500, 20000);
+        maximumClient = builder.defineInRange("maximumClient", 8500, 2500, 25000);
 
-        builder.comment("Set the server minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
+        builder.comment("Set the servers minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
         minimumServer = builder.defineInRange("minimumServer", 2500, 2500, 10000);
 
-        builder.comment("Set the server maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
+        builder.comment("Set the servers maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
                           + "recommended is 6000mb then set this to ~8000mb");
-        maximumServer = builder.defineInRange("maximumServer", 8500, 2500, 20000);
+        maximumServer = builder.defineInRange("maximumServer", 8500, 2500, 25000);
 
         builder.comment("Disable the memory warnings, default = false");
         disableWarnings = builder.define("disableWarnings", false);
 
-        builder.comment("Set the help message for changing ram allocation");
+        builder.comment("Set the help message for changing ram allocation, you can use html here e.g. for setting a link to a wiki page");
         helpfullinkmessage = builder.define("helpfullinkmessage",
           "See <a href='https://minecrafthelp.co.uk/how-to-add-more-ram-to-minecraft/'>https://minecrafthelp.co.uk/how-to-add-more-ram-to-minecraft/</a> on how to change your memory settings.");
 
