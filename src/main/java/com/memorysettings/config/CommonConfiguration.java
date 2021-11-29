f1353b66349fd9c7b0cf8017cf598ce828389d71
someaddons <someaddons@github.com>
2021-11-29 20:26:51 +0100

    v1.9: Up max config limits

--- src/main/java/com/memorysettings/config/CommonConfiguration.java
+++ src/main/java/com/memorysettings/config/CommonConfiguration.java
@@ -17,18 +17,18 @@ public class CommonConfiguration
         builder.push("Config category");
 
         builder.comment("Set the clients minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
-        minimumClient = builder.defineInRange("minimumClient", 2500, 2500, 10000);
+        minimumClient = builder.defineInRange("minimumClient", 2500, 2500, 25000);
 
         builder.comment("Set the clients maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
                           + "recommended is 6000mb then set this to ~8000mb");
-        maximumClient = builder.defineInRange("maximumClient", 8500, 2500, 25000);
+        maximumClient = builder.defineInRange("maximumClient", 8500, 2500, 60000);
 
         builder.comment("Set the servers minimum memory warning threshold in MB. Choose the lowest value possible which keeps the pack playable.");
-        minimumServer = builder.defineInRange("minimumServer", 2500, 2500, 10000);
+        minimumServer = builder.defineInRange("minimumServer", 2500, 2500, 25000);
 
         builder.comment("Set the servers maximum memory warning threshold in MB. Choose a generous maximum with some additional over the required, e.g."
                           + "recommended is 6000mb then set this to ~8000mb");
-        maximumServer = builder.defineInRange("maximumServer", 8500, 2500, 25000);
+        maximumServer = builder.defineInRange("maximumServer", 8500, 2500, 600000);
 
         builder.comment("Disable the memory warnings, default = false");
         disableWarnings = builder.define("disableWarnings", false);
