e2c3d8665204b3192355c06ead88e461333eb0a2
someaddons <someaddons@github.com>
2023-06-01 00:09:51 +0200

    4.2: Fix loading issues

---
+++ src/main/java/com/memorysettings/Memory.java
@@ -0,0 +1,17 @@
+package com.memorysettings;
+
+import java.lang.management.ManagementFactory;
+
+public class Memory
+{
+    public static int systemMemory = 0;
+    public static int freeMemory   = 0;
+    public static int heapSetting  = 0;
+
+    public static void recordMemory()
+    {
+        systemMemory = (int) (((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalMemorySize() / 1048576);
+        freeMemory = (int) (((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getFreeMemorySize() / 1048576);
+        heapSetting = (int) (ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax() / 1048576);
+    }
+}
