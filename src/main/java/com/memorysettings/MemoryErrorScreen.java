477938610a182080065cb1e0cb13325af666ae0f
someaddons <someaddons@github.com>
2023-06-04 13:47:03 +0200

    4.4: Add packmenu compat

--- src/main/java/com/memorysettings/MemoryErrorScreen.java
+++ src/main/java/com/memorysettings/MemoryErrorScreen.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.FormattedCharSequence;
+import net.minecraftforge.common.MinecraftForge;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -29,6 +30,7 @@ public class MemoryErrorScreen extends Screen
         super.init();
 
         button_proceed = new Button(this.width / 2 - 100, 140, 200, 20, CommonComponents.GUI_PROCEED, (button) -> {
+            MinecraftForge.EVENT_BUS.start();
             this.minecraft.setScreen((Screen) null);
         });
 
@@ -45,6 +47,7 @@ public class MemoryErrorScreen extends Screen
         });
 
         button_noremind = new Button(this.width / 2 - 100, 160, 200, 20, Component.translatable("button.stopremind"), (button) -> {
+            MinecraftForge.EVENT_BUS.start();
             this.minecraft.setScreen((Screen) null);
             MemorysettingsMod.config.getCommonConfig().disableWarnings.set(true);
         });
