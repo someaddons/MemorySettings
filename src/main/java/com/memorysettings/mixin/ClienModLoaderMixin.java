77fa613b4b30b7780c65a9bec848e0aba01be9c2
someaddons <someaddons@github.com>
2023-06-10 14:05:34 +0200

    4.5: Inject into forge

---
+++ src/main/java/com/memorysettings/mixin/ClienModLoaderMixin.java
@@ -0,0 +1,24 @@
+package com.memorysettings.mixin;
+
+import com.memorysettings.MemoryErrorScreen;
+import com.memorysettings.MemorysettingsMod;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.loading.ClientModLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientModLoader.class)
+public class ClienModLoaderMixin
+{
+    @Inject(method = "completeModLoading", at = @At("RETURN"), cancellable = true, remap = false)
+    private static void onReturn(final CallbackInfoReturnable<Boolean> cir)
+    {
+        if (!cir.getReturnValue())
+        {
+            Minecraft.getInstance().setScreen(new MemoryErrorScreen(MemorysettingsMod.memorycheckresult));
+            cir.setReturnValue(true);
+        }
+    }
+}
