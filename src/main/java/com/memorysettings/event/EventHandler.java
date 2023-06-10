7cd5406cf99dfbfd2df6367776980be86454152e
someaddons <someaddons@github.com>
2023-06-10 13:53:30 +0200

    4.5: Switch to event based loading

--- src/main/java/com/memorysettings/event/EventHandler.java
+++ src/main/java/com/memorysettings/event/EventHandler.java
@@ -1,8 +1,24 @@
 package com.memorysettings.event;
 
+import com.memorysettings.MemoryErrorScreen;
+import com.memorysettings.MemorysettingsMod;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
 /**
  * Forge event bus handler, ingame events are fired here
  */
 public class EventHandler
 {
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onGuiOpen(ScreenEvent.Opening event)
+    {
+        if (event.getNewScreen() instanceof TitleScreen && !MemorysettingsMod.memorycheckresult.getSiblings().isEmpty()
+              && !MemorysettingsMod.config.getCommonConfig().disableWarnings.get())
+        {
+            event.setNewScreen(new MemoryErrorScreen(MemorysettingsMod.memorycheckresult));
+        }
+    }
 }
