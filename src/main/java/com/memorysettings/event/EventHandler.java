2b518c292cc1cec5b4c6cd539948259bbd1404cf
someaddons <someaddons@github.com>
2023-06-10 14:05:34 +0200

    4.5: Inject into forge

--- src/main/java/com/memorysettings/event/EventHandler.java
+++ src/main/java/com/memorysettings/event/EventHandler.java
@@ -1,8 +1,5 @@
 package com.memorysettings.event;
 
-import com.memorysettings.MemoryErrorScreen;
-import com.memorysettings.MemorysettingsMod;
-import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,12 +10,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class EventHandler
 {
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void onGuiOpen(ScreenEvent.Opening event)
+    public static void onGuiOpen(ScreenEvent.Opening event)
     {
-        if (event.getNewScreen() instanceof TitleScreen && !MemorysettingsMod.memorycheckresult.getSiblings().isEmpty()
-              && !MemorysettingsMod.config.getCommonConfig().disableWarnings.get())
-        {
-            event.setNewScreen(new MemoryErrorScreen(MemorysettingsMod.memorycheckresult));
-        }
+
     }
 }
