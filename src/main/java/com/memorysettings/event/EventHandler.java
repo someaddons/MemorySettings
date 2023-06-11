839d3e34ea3cd6f33ded71f75e7ffa92bd959948
someaddons <someaddons@github.com>
2023-06-11 10:28:11 +0200

    4.6: Fix serverside loading

--- src/main/java/com/memorysettings/event/EventHandler.java
+++ src/main/java/com/memorysettings/event/EventHandler.java
@@ -1,17 +1,8 @@
 package com.memorysettings.event;
 
-import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
 /**
  * Forge event bus handler, ingame events are fired here
  */
 public class EventHandler
 {
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onGuiOpen(ScreenEvent.Opening event)
-    {
-
-    }
 }
